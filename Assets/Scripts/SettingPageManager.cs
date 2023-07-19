be135060c2df9760764135adec3ac4a8ea30ec6d
Hierarch <whitekudari@gmail.com>
2023-07-19 17:47:14 +0900

    2023.7.19 파일 위치 수정

--- Assets/Scripts/SettingPageManager.cs
+++ Assets/Scripts/SettingPageManager.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
+public static class UserSettingSave
+{
+    public static float gamma = 1;
+    public static float audio_Main = 0;
+    public static float audio_Sfx = 0;
+    //public static float audio_Music;
+}
 public class SettingPageManager : MonoBehaviour
 {
     public GameObject SettingPage;
     public TimerManager TimerManager;
+    public AudioMixer MasterVolume;
+    public Slider MasterSlider;
+    public Slider SFXSlider;
+    public Slider GammaSlider;
+    public CanvasGroup GammaImage;
+
     void Start()
     {
         SettingPage.SetActive(false);
+        AudioControl();
+        GammaControl();
     }
 
     void Update()
@@ -23,6 +41,30 @@ public class SettingPageManager : MonoBehaviour
             SettingPage.SetActive(true);
         }
     }
+
+    public float Lerp(float s, float e, float t)
+    {
+        return s + (e - s) * t;
+    }
+    public void AudioControl()
+    {
+        UserSettingSave.audio_Main = Lerp(-40, 10, MasterSlider.value);
+        MasterVolume.SetFloat("Volume_Main", UserSettingSave.audio_Main);
+
+        UserSettingSave.audio_Sfx = Lerp(-40, 10, SFXSlider.value);
+        MasterVolume.SetFloat("Volume_SFX", UserSettingSave.audio_Sfx);
+
+        Debug.Log("Volume Change");
+        //UserSettingSave.audio_Music = MusicSlider.value; //--> BGM 삽입 이후 슬라이더 추가 및 설정
+        //MasterVolume.SetFloat("Volume_Music", UserSettingSave.audio_Music);
+    }
+
+    public void GammaControl()
+    {
+        UserSettingSave.gamma = GammaSlider.value;
+        GammaImage.alpha = 1 - UserSettingSave.gamma;
+    }
+
     public void SettingPageOff() //세팅 페이지의 나가기 버튼에 할당
     {
         TimerManager.timerRestart();
