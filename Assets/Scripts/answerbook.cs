1b5608e9c28f1de61a4f3bed2f6faa8f3707b039
kn99e39 <dna10165@gmail.com>
2023-08-20 02:00:13 +0900

    주문서 이슈

--- Assets/Scripts/answerbook.cs
+++ Assets/Scripts/answerbook.cs
@@ -6,12 +6,23 @@ public class answerbook : MonoBehaviour
 {
     public GameObject answerBook;
     public CanvasGroup answerBookGroup;
+    public GameObject SubmitBtn;
+    public GameObject SubmitField;
+
+    private void Start()
+    {
+        SubmitBtn.SetActive(false);
+        SubmitField.SetActive(false);
+    }
+
     private void OnMouseDown()
     {
         if(SettingPageManager.GamePaused == false && BookSwitch.BookOn == false)
         {
             BookSwitch.BookOn = true;
             answerBookGroup.alpha = 1;
+            SubmitBtn.SetActive (true);
+            SubmitField.SetActive (true);
         }
         
     }
@@ -22,6 +33,8 @@ public class answerbook : MonoBehaviour
         {
             answerBookGroup.alpha = 0;
             BookSwitch.BookOn = false;
+            SubmitBtn.SetActive(false);
+            SubmitField.SetActive(false);
         }
     }
 }
